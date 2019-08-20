88ae28b737c1f7f674088c21f22b2773513885fe
huangwei <harw.op@qq.com>
2019-08-20 11:17:08 +0800

    initial test setup(TDD)

---
+++ tutorial01_exercise/tutorial01_exercise/leptjson.c
@@ -0,0 +1,17 @@
+//
+//  leptjson.c
+//  tutorial01_exercise
+//
+//  Created by GoSwak on 2019/8/20.
+//  Copyright © 2019 huangwei. All rights reserved.
+//
+
+#include "leptjson.h"
+
+int lept_parse(lept_value* v, const char* json) {
+  return -1;
+}
+
+lept_type lept_get_type(const lept_value* v) {
+  return -1;
+}
---
+++ tutorial01_exercise/tutorial01_exercise/leptjson.h
@@ -0,0 +1,47 @@
+//
+//  leptjson.h
+//  tutorial01_exercise
+//
+//  Created by GoSwak on 2019/8/20.
+//  Copyright © 2019 huangwei. All rights reserved.
+//
+
+#ifndef leptjson_h
+#define leptjson_h
+
+
+typedef enum {
+  LEPT_NULL,
+  LEPT_FALSE,
+  LEPT_TRUE,
+  LEPT_NUMBER,
+  LEPT_STRING,
+  LEPT_ARRAY,
+  LEPT_OBJECT,
+} lept_type;
+
+enum {
+  LEPT_PARSE_OK = 0,
+  LEPT_PARSE_EXPECT_VALUE,
+  LEPT_PARSE_INVALID_VALUE,
+  LEPT_PARSE_ROOT_NOT_SINGULAR,
+};
+
+typedef struct {
+  lept_type type;
+} lept_value;
+
+int lept_parse(lept_value* v, const char* json);
+
+lept_type lept_get_type(const lept_value* v);
+
+/**
+ JSON-text = ws value ws
+ ws = *(%x20 / %x09 / %x0A / %x0D)
+ value = null / false / true 
+ null  = "null"
+ false = "false"
+ true  = "true"
+ */
+
+#endif /* leptjson_h */
